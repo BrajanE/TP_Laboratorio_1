4188e68ddcb64154c26cdabb80f24eac14671970
BrajanE <estrada.brajan@gmail.com>
2021-10-16 23:48:01 -0300

    TP-2

--- TP-2/src/TP-2.c
+++ TP-2/src/TP-2.c
@@ -19,6 +19,7 @@ int main(void) {
 	int opcion;
 	int id;
 	id=0;
+	int flagMenu=0;
 	int corroboracion;
 	int idAuxiliar;
 	float totalSalario=0;
@@ -37,8 +38,14 @@ int main(void) {
 			id=IdAutomatico(id);
 			corroboracion=ingresarEmpleado(listadoEmpleados, TAM_EMP,id);
 			MensajeDeAlerta(corroboracion, "Se dio de alta con exito.","Hubo un error al dar de alta.");
+			flagMenu = 1;
 			break;
 		case 2:
+			if(flagMenu ==0)
+			{
+				printf("Debe dar de alta 1 empleado.\n\n");
+			} else if(flagMenu==1)
+			{
 			corroboracion= ModificarEmpleadoPorId (listadoEmpleados, TAM_EMP);
 			if(corroboracion !=-1)
 			{
@@ -47,48 +54,61 @@ int main(void) {
 			{
 				printf("Error al modificar, es probable que no se encuentre el ID.\n\n");
 			}
-			break;
-		case 3:
-			printEmployees(listadoEmpleados, TAM_EMP);
-			idAuxiliar = PedirEnteroFull("Ingrese ID de empleado a remover: ", TAM_EMP);
-			corroboracion = findEmployeeById(listadoEmpleados,TAM_EMP,idAuxiliar);
-			if(corroboracion!= -1)
-			{
-				removeEmployee(listadoEmpleados,TAM_EMP,idAuxiliar);
-				printf("Baja con exito.\n\n");
-			} else{
-				printf("No se encuentra el ID del empleado.\n\n");
 			}
+			break;
 
+		case 3:
+			if(flagMenu ==0)
+			{
+				printf("Debe dar de alta 1 empleado.\n\n");
+			} else if(flagMenu==1)
+			{
+				printEmployees(listadoEmpleados, TAM_EMP);
+				idAuxiliar = PedirEnteroFull("Ingrese ID de empleado a remover: ", TAM_EMP);
+				corroboracion = findEmployeeById(listadoEmpleados,TAM_EMP,idAuxiliar);
+				if(corroboracion!= -1)
+				{
+					removeEmployee(listadoEmpleados,TAM_EMP,idAuxiliar);
+					printf("Baja con exito.\n\n");
+				} else{
+				printf("No se encuentra el ID del empleado.\n\n");
+				}
+			}
 			break;
 		case 4:
-			MostrarSegundoMenu();
-			opcion=PedirEnteroFull("Ingrese opcion: \n",2);
-			switch (opcion)
+			if(flagMenu ==0)
 			{
-			case 1:
-				opcion=PedirEnteroFull("1- Ordenar por apellido.\n2- Ordenar por sector.\n", 2);
-				corroboracion=sortEmployees(listadoEmpleados,TAM_EMP, opcion);
-				if(corroboracion!=-1)
+				printf("Debe dar de alta 1 empleado.\n\n");
+			} else if(flagMenu==1)
+			{
+				MostrarSegundoMenu();
+				opcion=PedirEnteroFull("Ingrese opcion: \n",2);
+				switch (opcion)
 				{
-					printf("Ordenado exitosamente:\n");
-					printEmployees(listadoEmpleados, TAM_EMP);
-				} else {
-					printf("Error.");
-				}
-				break;
-			case 2:
-				corroboracion=CalculosConSalarios(listadoEmpleados,TAM_EMP,&totalSalario,&cantEmpleados);
-				if(corroboracion!=-1 && cantEmpleados!=0)
-				{
-					promSalario=totalSalario/cantEmpleados;
-					empleadosArribaDelPromedio=EmpleadosConMayorSalario(listadoEmpleados,TAM_EMP,promSalario);
-					printf("El total de los sarios es: %.2f.\nLa cantidad de empleados son: %d.\nEl promedio es: %.2f.\nLa cantidad de empleados que superan el sueldo promedio son: %d\n\n",totalSalario,cantEmpleados,promSalario,empleadosArribaDelPromedio);
+				case 1:
+					opcion=PedirEnteroFull("1- Ordenar por apellido.\n2- Ordenar por sector.\n", 2);
+					corroboracion=sortEmployees(listadoEmpleados,TAM_EMP, opcion);
+					if(corroboracion!=-1)
+					{
+						printf("Ordenado exitosamente:\n");
+						printEmployees(listadoEmpleados, TAM_EMP);
+					} else {
+						printf("Error.");
+					}
+					break;
+				case 2:
+					corroboracion=CalculosConSalarios(listadoEmpleados,TAM_EMP,&totalSalario,&cantEmpleados);
+					if(corroboracion!=-1 && cantEmpleados!=0)
+					{
+						promSalario=totalSalario/cantEmpleados;
+						empleadosArribaDelPromedio=EmpleadosConMayorSalario(listadoEmpleados,TAM_EMP,promSalario);
+						printf("El total de los sarios es: %.2f.\nLa cantidad de empleados son: %d.\nEl promedio es: %.2f.\nLa cantidad de empleados que superan el sueldo promedio son: %d\n\n",totalSalario,cantEmpleados,promSalario,empleadosArribaDelPromedio);
 
-				} else{
-					printf("No se pudieron realizar los calculos correspondientes.\n");
+					} else{
+						printf("No se pudieron realizar los calculos correspondientes.\n");
+					}
+					break;
 				}
-				break;
 			}
 			break;
 
